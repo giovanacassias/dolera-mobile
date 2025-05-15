d3127bc03338e521b9b3c0e6a57648e7c49f88ab
Giovana Cassias <giovanacassias@outlook.com>
2025-05-14 22:04:59 -0300

    bugs fixed - ready to start

---
+++ dolera-app-mobile/app/_layout.tsx
@@ -0,0 +1,13 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import { Stack } from "expo-router";
+
+export default function _layout() {
+  return <Stack />;
+}
+
+const styles = StyleSheet.create({
+  general: {
+    backgroundColor: "#FAFAFA",
+  },
+});
---
+++ dolera-app-mobile/app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text, StyleSheet, TextProps } from "react-native";
+import React from "react";
+import Header from "../components/Header";
+
+export default function index() {
+  return <Header></Header>;
+}
+
+const styles = StyleSheet.create({});
