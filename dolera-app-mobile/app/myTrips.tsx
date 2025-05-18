8f1bfd86eb7dd5aeab8361a118aeb7dd0d1caa16
Giovana Cassias <giovanacassias@outlook.com>
2025-05-17 23:20:27 -0300

    third-sceen started

--- dolera-app-mobile/app/myTrips.tsx
+++ dolera-app-mobile/app/myTrips.tsx
@@ -1,19 +1,24 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import { useLocalSearchParams } from "expo-router";
+import Flex from "../components/Flex";
+import TitleH1 from "../components/TitleH1";
+import Header from "../components/Header";
 
 export default function myTrips() {
   const params = useLocalSearchParams();
   const trip = params.trip ? JSON.parse(params.trip as string) : null;
 
   return (
-    <View>
-      <Text>My Trips</Text>
-      <Text>Name: {trip.name}</Text>
-      <Text>Currency: {trip.currency}</Text>
-      <Text>Start Date: {trip.startDate}</Text>
-      <Text>Finish Date: {trip.endDate}</Text>
-      <Text>Budget: {trip.budget}</Text>
-    </View>
+    <Flex style={styles.container}>
+      <Header />
+      <TitleH1 title="Caio's trips" />
+    </Flex>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 20,
+  },
+});
