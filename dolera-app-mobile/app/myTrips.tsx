a15b268a2801a006c565e0025e83839bba958129
Giovana Cassias <giovanacassias@outlook.com>
2025-05-17 23:33:43 -0300

    starting TripCard

--- dolera-app-mobile/app/myTrips.tsx
+++ dolera-app-mobile/app/myTrips.tsx
@@ -4,6 +4,8 @@ import { useLocalSearchParams } from "expo-router";
 import Flex from "../components/Flex";
 import TitleH1 from "../components/TitleH1";
 import Header from "../components/Header";
+import { Avatar, Card } from "react-native-paper";
+import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
 
 export default function myTrips() {
   const params = useLocalSearchParams();
@@ -13,6 +15,7 @@ export default function myTrips() {
     <Flex style={styles.container}>
       <Header />
       <TitleH1 title="Caio's trips" />
+      <MaterialIcons name="flight" size={32} color="black" />
     </Flex>
   );
 }
---
+++ dolera-app-mobile/components/TripCard.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import React from "react";
+import Flex from "./Flex";
+
+export default function TripCard() {
+  return (
+    <View>
+      <Text>TripCard</Text>
+    </View>
+  );
+}
